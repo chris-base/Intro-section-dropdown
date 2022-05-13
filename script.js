babe191308b2a8d602fee5ce4681aa396166c165
chris-base <baseappsdev@gmail.com>
2022-05-13 14:18:58 -0500

    started dropdown from header layouts

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.getElementById("headerMenuDropdown").addEventListener("click", function
   document.getElementById("drawerContainer").style.display = "flex";
 });
 
+document.getElementById("drawerCloseButton").addEventListener("click", function (event) {
+  document.getElementById("drawerContainer").style.display = "none";
+});
+
 document.addEventListener("click", function (event) {
   var isClickInside =
     document.getElementById("drawerWhiteContainer").contains(event.target) || document.getElementById("headerMenuDropdown").contains(event.target);
